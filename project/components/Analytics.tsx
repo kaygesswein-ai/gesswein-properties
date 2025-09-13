b46ab35d229866ed61e1c2daf8346fcb0ea7c2da
kaygesswein-ai <kaygesswein@gmail.com>
2025-09-13 13:25:08 -0300

    Update Analytics.tsx

--- project/components/Analytics.tsx
+++ project/components/Analytics.tsx
@@ -1,3 +1,4 @@
+// project/components/Analytics.tsx
 'use client'
 
 import Script from 'next/script'
@@ -6,14 +7,14 @@ import { GA_TRACKING_ID, META_PIXEL_ID } from '@/lib/analytics'
 export default function Analytics() {
   return (
     <>
-      {/* Google Analytics */}
-      {GA_TRACKING_ID && (
+      {/* Google Analytics (solo si hay ID) */}
+      {GA_TRACKING_ID ? (
         <>
           <Script
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
             strategy="afterInteractive"
           />
-          <Script id="google-analytics" strategy="afterInteractive">
+          <Script id="ga" strategy="afterInteractive">
             {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
@@ -22,25 +23,32 @@ export default function Analytics() {
             `}
           </Script>
         </>
-      )}
+      ) : null}
 
-      {/* Meta Pixel */}
-      {META_PIXEL_ID && (
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${META_PIXEL_ID}');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-      )}
+      {/* Meta Pixel (solo si hay ID) */}
+      {META_PIXEL_ID ? (
+        <>
+          <Script id="fb-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s){
+                if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)
+              }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${META_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1"/>`,
+            }}
+          />
+        </>
+      ) : null}
     </>
   )
 }
